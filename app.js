bd23b28f09314987bce883d42d669dd3febee3fc
arleyrodriguez077-lab <arleyrodriguez077@gmail.com>
2026-05-22 15:25:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,31 +12,3 @@ const firebaseConfig = {
   appId: "TU_APP_ID"
 };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDYr2Zgk4njVPcR--P3vUUpgsr5e-uk6e0",
-    authDomain: "nodoo-29224.firebaseapp.com",
-    projectId: "nodoo-29224",
-    storageBucket: "nodoo-29224.firebasestorage.app",
-    messagingSenderId: "454908786832",
-    appId: "1:454908786832:web:e829f4ceb90a1fed0e4015",
-    measurementId: "G-2QH34EQ96F"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-
-
